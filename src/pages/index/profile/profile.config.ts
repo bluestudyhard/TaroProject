611de0c56bdbaf6e39b6be049222e389bdd06c9b
bluestudyhard <3164752596@qq.com>
2023-07-22 23:12:12 +0800

    chore: Initial commit - add Taro + vue +ts + vant-weapp template

--- src/pages/index/profile/profile.config.ts
+++
@@ -1,7 +0,0 @@
-export default definePageConfig({
-  navigationBarTitleText: "个人中心",
-  enableShareAppMessage: true,
-  usingComponents: {
-    "van-button": "@/components/vant-weapp/button/index",
-  },
-});
